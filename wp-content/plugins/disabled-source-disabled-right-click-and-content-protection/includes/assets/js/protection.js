decc050b7230fbaf8cf11871c16e31d0a8392088
localizermeerkat <localizermeerkat@hotmail.com>
2026-02-02 04:30:44 +0700

    Update from WordPress

--- wp-content/plugins/disabled-source-disabled-right-click-and-content-protection/includes/assets/js/protection.js
+++ wp-content/plugins/disabled-source-disabled-right-click-and-content-protection/includes/assets/js/protection.js
@@ -14,159 +14,95 @@ if (jh_disabled_options_data.disabled_click == "1") {
       e.preventDefault();
    });
 }
+function isCtrlOrCmd(e) {
+   return e.ctrlKey || e.metaKey;
+}
+
+function notify() {
+   if (
+       jh_disabled_options_data.disabled_notifi_status == "1" &&
+       jh_disabled_options_data.disabled_notifi_text !== ""
+   ) {
+       jQuery.notify(jh_disabled_options_data.disabled_notifi_text, {
+           position: jh_disabled_options_data.disabled_notifi_position,
+           className: 'error',
+           showAnimation: 'fadeIn',
+           hideAnimation: 'fadeOut',
+           style: 'bootstrap'
+       });
+   }
+}
+
+document.addEventListener('keydown', function (e) {
+
+   const key = e.key.toUpperCase();
+
+   // F12
+   if (jh_disabled_options_data.disabled_f12 == "1" && key === "F12") {
+       notify();
+       e.preventDefault();
+       return false;
+   }
+
+   // Ctrl/Cmd + I  OR  Ctrl/Cmd + Shift + I
+   if (jh_disabled_options_data.disabled_ctst_i == "1") {
+       if (isCtrlOrCmd(e) && key === 'I') {
+           notify();
+           e.preventDefault();
+           return false;
+       }
+   }
+
+   // Ctrl/Cmd + C  OR  Ctrl/Cmd + Shift + C
+   if (jh_disabled_options_data.disabled_ctst_c == "1") {
+       if (isCtrlOrCmd(e) && key === 'C') {
+           notify();
+           e.preventDefault();
+           return false;
+       }
+   }
+
+   // Ctrl/Cmd + J  OR  Ctrl/Cmd + Shift + J
+   if (jh_disabled_options_data.disabled_ctst_j == "1") {
+       if (isCtrlOrCmd(e) && key === 'J') {
+           notify();
+           e.preventDefault();
+           return false;
+       }
+   }
+
+   // Ctrl/Cmd + U (View Source)
+   if (jh_disabled_options_data.disabled_ct_u == "1") {
+       if (isCtrlOrCmd(e) && key === 'U') {
+           notify();
+           e.preventDefault();
+           return false;
+       }
+   }
+
+   // Ctrl/Cmd + P (Print)
+   if (jh_disabled_options_data.disabled_ct_p == "1") {
+       if (isCtrlOrCmd(e) && key === 'P') {
+           notify();
+           e.preventDefault();
+           return false;
+       }
+   }
+
+   // Ctrl/Cmd + S (Save)
+   if (jh_disabled_options_data.disabled_ct_s == "1") {
+       if (isCtrlOrCmd(e) && key === 'S') {
+           notify();
+           e.preventDefault();
+           return false;
+       }
+   }
+});
 
 document.onkeydown = function(e) {
-   if (jh_disabled_options_data.disabled_f12 == "1") {
-      if (event.keyCode == 123) {
-         if(jh_disabled_options_data.disabled_notifi_status=="1" && jh_disabled_options_data.disabled_notifi_text!=""){
-            jQuery.notify(jh_disabled_options_data.disabled_notifi_text,
-               {
-               position: jh_disabled_options_data.disabled_notifi_position,
-               className: 'error',  
-               showAnimation: 'fadeIn', 
-               hideAnimation: 'fadeOut', 
-               style: 'bootstrap'
-               }
-            );
-         }
-         return false;
-      }
-   }
-   if (jh_disabled_options_data.disabled_ctst_i == "1") {
-      if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-         if(jh_disabled_options_data.disabled_notifi_status=="1" && jh_disabled_options_data.disabled_notifi_text!=""){
-            jQuery.notify(jh_disabled_options_data.disabled_notifi_text,
-               {
-               position: jh_disabled_options_data.disabled_notifi_position,
-               className: 'error',  
-               showAnimation: 'fadeIn', 
-               hideAnimation: 'fadeOut', 
-               style: 'bootstrap'
-               }
-            );
-         }
-         return false;
-      }
-      if (e.ctrlKey && e.keyCode == 'I'.charCodeAt(0)) {
-         if(jh_disabled_options_data.disabled_notifi_status=="1" && jh_disabled_options_data.disabled_notifi_text!=""){
-            jQuery.notify(jh_disabled_options_data.disabled_notifi_text,
-               {
-               position: jh_disabled_options_data.disabled_notifi_position,
-               className: 'error',  
-               showAnimation: 'fadeIn', 
-               hideAnimation: 'fadeOut', 
-               style: 'bootstrap'
-               }
-            );
-         }
-         return false;
-      }
-   }
-   if (jh_disabled_options_data.disabled_ctst_c == "1") {
-      if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-         if(jh_disabled_options_data.disabled_notifi_status=="1" && jh_disabled_options_data.disabled_notifi_text!=""){
-            jQuery.notify(jh_disabled_options_data.disabled_notifi_text,
-               {
-               position: jh_disabled_options_data.disabled_notifi_position,
-               className: 'error',  
-               showAnimation: 'fadeIn', 
-               hideAnimation: 'fadeOut', 
-               style: 'bootstrap'
-               }
-            );
-         }
-         return false;
-      }
-      if (e.ctrlKey && e.keyCode == 'C'.charCodeAt(0)) {
-         if(jh_disabled_options_data.disabled_notifi_status=="1" && jh_disabled_options_data.disabled_notifi_text!=""){
-            jQuery.notify(jh_disabled_options_data.disabled_notifi_text,
-               {
-               position: jh_disabled_options_data.disabled_notifi_position,
-               className: 'error',  
-               showAnimation: 'fadeIn', 
-               hideAnimation: 'fadeOut', 
-               style: 'bootstrap'
-               }
-            );
-         }
-         return false;
-      }
-   }
-   if (jh_disabled_options_data.disabled_ctst_j == "1") {
-      if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-         if(jh_disabled_options_data.disabled_notifi_status=="1" && jh_disabled_options_data.disabled_notifi_text!=""){
-            jQuery.notify(jh_disabled_options_data.disabled_notifi_text,
-               {
-               position: jh_disabled_options_data.disabled_notifi_position,
-               className: 'error',  
-               showAnimation: 'fadeIn', 
-               hideAnimation: 'fadeOut', 
-               style: 'bootstrap'
-               }
-            );
-         }
-         return false;
-      }
-      if (e.ctrlKey && e.keyCode == 'J'.charCodeAt(0)) {
-         if(jh_disabled_options_data.disabled_notifi_status=="1" && jh_disabled_options_data.disabled_notifi_text!=""){
-            jQuery.notify(jh_disabled_options_data.disabled_notifi_text,
-               {
-               position: jh_disabled_options_data.disabled_notifi_position,
-               className: 'error',  
-               showAnimation: 'fadeIn', 
-               hideAnimation: 'fadeOut', 
-               style: 'bootstrap'
-               }
-            );
-         }
-         return false;
-      }
-   }
-   if (jh_disabled_options_data.disabled_ct_u == "1") {
-      if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-         if(jh_disabled_options_data.disabled_notifi_status=="1" && jh_disabled_options_data.disabled_notifi_text!=""){
-            jQuery.notify(jh_disabled_options_data.disabled_notifi_text,
-               {
-               position: jh_disabled_options_data.disabled_notifi_position,
-               className: 'error',  
-               showAnimation: 'fadeIn', 
-               hideAnimation: 'fadeOut', 
-               style: 'bootstrap'
-               }
-            );
-         }
-         return false;
-      }
-   }
    if (jh_disabled_options_data.disabled_ct_p == "1") {
       if (e.ctrlKey && e.keyCode == 'P'.charCodeAt(0)) {
-         if(jh_disabled_options_data.disabled_notifi_status=="1" && jh_disabled_options_data.disabled_notifi_text!=""){
-            jQuery.notify(jh_disabled_options_data.disabled_notifi_text,
-               {
-               position: jh_disabled_options_data.disabled_notifi_position,
-               className: 'error',  
-               showAnimation: 'fadeIn', 
-               hideAnimation: 'fadeOut', 
-               style: 'bootstrap'
-               }
-            );
-         }
-         return false;
-      }
-   }
-   if (jh_disabled_options_data.disabled_ct_s == "1") {
-      if (e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
-         if(jh_disabled_options_data.disabled_notifi_status=="1" && jh_disabled_options_data.disabled_notifi_text!=""){
-            jQuery.notify(jh_disabled_options_data.disabled_notifi_text,
-               {
-               position: jh_disabled_options_data.disabled_notifi_position,
-               className: 'error',  
-               showAnimation: 'fadeIn', 
-               hideAnimation: 'fadeOut', 
-               style: 'bootstrap'
-               }
-            );
-         }
+         notify();
          return false;
       }
    }
